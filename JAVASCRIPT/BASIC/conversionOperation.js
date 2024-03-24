5d47cc7093f623f232f268333dc891b08d7d9233
Vaibhav Gautam <vaibhavlovescode@gmail.com>
2024-03-24 16:34:38 +0530

    JS IN PROGRESS

---
+++ JAVASCRIPT/BASIC/conversionOperation.js
@@ -0,0 +1,45 @@
+// let score = "VAIBHAV";
+// console.log(typeof(score));
+
+// let valueinNumber = Number(score);
+// console.log(typeof(valueinNumber));
+// console.log(valueinNumber);
+
+
+// let someNumber  = 44;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+// let str1 = "hello"
+// let str2 = " Vaibhav"
+
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+console.log("2" === 2);
---
+++ JAVASCRIPT/BASIC/string.js
@@ -0,0 +1,26 @@
+const gameName = new String('VAIBHAV-GAUTAM-HI');
+
+// console.log(gameName.toLocaleUpperCase());
+
+// console.log(gameName.charAt(2));
+
+// const newString = gameName.substring(0,4);
+
+// console.log(newString);
+
+// const testString = new String("   vaibhav ");
+
+// const trimmedString = testString.trim();
+
+// const upperCasedString = trimmedString.toLocaleUpperCase();
+
+// console.log(upperCasedString);
+
+const url = new String("https://vaibhav.com/vaibhav%20gautam");
+
+console.log(url.replace('%20','-'))
+
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));

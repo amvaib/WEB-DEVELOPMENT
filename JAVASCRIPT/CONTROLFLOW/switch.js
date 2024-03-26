2ccb11d15ff800f8d4f876bbdf843f99069cda08
Vaibhav Gautam <vaibhavlovescode@gmail.com>
2024-03-26 15:11:45 +0530

    SWITCH CASE ADDED

---
+++ JAVASCRIPT/CONTROLFLOW/switch.js
@@ -0,0 +1,44 @@
+const month = 19;
+
+switch (month) {
+    case 1:
+        console.log("JANUARY");
+        break;
+    case 2:
+        console.log("FEBRUARY");
+        break;
+    case 3:
+        console.log("MARCH");
+        break;
+    case 4:
+        console.log("APRIL");
+        break;
+    case 5:
+        console.log("MAY");
+        break;
+    case 6:
+        console.log("JUNE");
+        break;
+    case 7:
+        console.log("JULY");
+        break;
+    case 8:
+        console.log("AUGUST");
+        break;
+    case 9:
+        console.log("SEPTEMBER");
+        break;
+    case 10:
+        console.log("OCTOBER");
+        break;
+    case 12:
+        console.log("NOVEMBER");
+        break;
+    case 12:
+        console.log("DECEMBER");
+        break;
+
+    default:
+        console.log("ENTER A VALID NUMBER FOR MONTH");
+        break;
+}

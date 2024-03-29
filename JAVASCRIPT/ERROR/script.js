3fed4ae16342817c9e25516ec5a0595389dd98ad
Vaibhav Gautam <vaibhavlovescode@gmail.com>
2024-03-29 22:31:20 +0530

    TRY AND CATCH ADDED

---
+++ JAVASCRIPT/ERROR/script.js
@@ -0,0 +1,17 @@
+setTimeout(()=>{
+    console.log("HACKING WIFI......... PLEASE WAIT");
+},1000)
+
+try{
+    console.log(rahul);
+} catch(error){
+    console.log("maa chudaye error");
+}
+
+setTimeout(()=>{
+    console.log("FETCHING USERNAME AND PASSWORD");
+},2000)
+setTimeout(()=>{
+    console.log("FETCHED");
+},5000)
+
